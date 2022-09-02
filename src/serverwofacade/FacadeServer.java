8dcc3862b6ad1f48edc4a0665718dfe4e2c84823
pwn233 <astiweiei@gmail.com>
2022-09-02 09:42:37 +0700

    push t o main

---
+++ src/serverwofacade/FacadeServer.java
@@ -0,0 +1,23 @@
+package serverwofacade;
+
+public class FacadeServer {
+    ScheduleServer scheduleServer = new ScheduleServer();
+    public void start() {
+        scheduleServer.startBooting();
+        scheduleServer.readSystemConfigFile();
+        scheduleServer.init();
+        scheduleServer.initializeContext();
+        scheduleServer.initializeListeners();
+        scheduleServer.createSystemObjects();
+        System.out.println("Start working......");
+    }
+    public void stop() {
+        System.out.println("After work done.........");
+        scheduleServer.releaseProcesses();
+        scheduleServer.destory();
+        scheduleServer.destroySystemObjects();
+        scheduleServer.destoryListeners();
+        scheduleServer.destoryContext();
+        scheduleServer.shutdown();
+    }
+}
